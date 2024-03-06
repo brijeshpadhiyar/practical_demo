8f66b03d7188efecb092c213030ca22a6e758e76
Brijesh Padhiyar <104890493+brijeshpadhiyar@users.noreply.github.com>
2024-03-06 20:34:30 +0530

    05-03

---
+++ lib/const/app_icons.dart
@@ -0,0 +1,9 @@
+class AppIcons {
+  static const String icons = "assets/icons";
+
+  static const String briefcase = "$icons/briefcase.svg";
+  static const String database = "$icons/database.svg";
+  static const String home = "$icons/home.svg";
+  static const String more = "$icons/More.svg";
+  static const String news = "$icons/News.svg";
+}
---
+++ lib/const/app_imges.dart
@@ -0,0 +1,25 @@
+class AppImages {
+  static const String images = "assets/images";
+
+  static const String user = "$images/user.jpeg";
+  static const String logoVector = "$images/logo_vector.png";
+  static const String bordTop = "$images/bord_top.jpeg";
+  static const String centerBord = "$images/center_bord.jpeg";
+
+  //categories
+  static const String c1 = "$images/c1.jpeg";
+  static const String c2 = "$images/c2.jpeg";
+  static const String c3 = "$images/c3.jpeg";
+  static const String c4 = "$images/c4.jpeg";
+
+  //export
+  static const String e1 = "$images/e1.jpeg";
+  static const String e2 = "$images/e2.jpeg";
+  static const String e3 = "$images/e3.jpeg";
+
+  //offer
+  static const String offer1 = "$images/offer1.jpeg";
+  static const String offer2 = "$images/offer2.jpeg";
+  static const String offer3 = "$images/offer3.jpeg";
+  static const String offer4 = "$images/offer4.jpeg";
+}
